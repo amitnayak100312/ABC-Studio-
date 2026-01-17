4c4190c1482272413dda67697d5ae8c7dfd52b67
amitnayak100312 <amitnayak121003@gmail.com>
2026-01-17 15:14:48 +0530

    Update WhatsAppController.php

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
 
 class WhatsAppController extends Controller
 { 
    public function sendmsg(){
-        
+        $twilioSid = env('T')
     }
 }
