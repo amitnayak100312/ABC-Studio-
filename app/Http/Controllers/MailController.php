01d3cf223eb07ec2c3be1aa869c2d32e8f0f750f
amitnayak100312 <amitnayak121003@gmail.com>
2026-01-17 14:43:48 +0530

    Update MailController.php

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,34 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail; 
+use Illuminate\Support\Facades\Mail;
 use App\Mail\mailApp;
 use App\Mail\mailReply;
 use App\Models\MailEntry;
+use App\Models\Review;
 use Twilio\Rest\Client;
 
 class MailController extends Controller
 {
-   function mailApp(Request $req){
-        
+    function mailApp(Request $req)
+    {
+
         // 1. Validate 'name' (Matches your HTML form name="name")
         $req->validate([
             'name'    => 'required|string|max:255',
             'email'   => 'required|email',
-            'service' => 'required', 
+            'service' => 'required',
             'message' => 'required|min:10'
         ]);
 
         // 2. Save to Database
         $mailDB = new MailEntry();
-        $mailDB->name = $req->name; 
+        $mailDB->name = $req->name;
         $mailDB->email = $req->email;
-        $mailDB->subject = $req->service; 
+        $mailDB->subject = $req->service;
         $mailDB->message = $req->message;
         $mailDB->save();
 
         // 3. Prepare Data
-        $to = "amitnayak121003@gmail.com"; 
+        $to = "amitnayak121003@gmail.com";
         $email = $req->email;
         $sub = $req->service;
         $msg = $req->message;
@@ -46,29 +48,31 @@ class MailController extends Controller
 
         return redirect()->back()->with('success', 'Thank you! We have received your message.');
     }
-    
-    public function viewMessages(){
+
+    public function viewMessages()
+    {
         $messages = MailEntry::orderBy('created_at', 'desc')->get();
         return view('back-end.viewClient', compact('messages'));
     }
-    
-    public function viewReview(){
-             $messages = MailEntry::orderBy('created_at', 'desc')->get();
-        return view('back-end.viewClient', compact('messages'));
-   
-    }
-    public function sendReply(Request $request){
-    $request->validate([
-        'email' => 'required|email',
-        'reply_message' => 'required'
-    ]);
-    
-    $email = $request->email;
-    $msg = $request->reply_message;
-    $sub = "Re: " . $request->original_subject;
-    $name = "Client"; 
 
-    Mail::to($email)->send(new mailReply($msg, $sub, $name, $email)); 
-    return back()->with('success', 'Reply sent successfully to ' . $email);
-}
+    public function viewReview()
+    {
+        $messages = Review::orderBy('created_at', 'desc')->get();
+        return view('back-end.viewreview', compact('messages'));
+    }
+    public function sendReply(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'reply_message' => 'required'
+        ]);
+
+        $email = $request->email;
+        $msg = $request->reply_message;
+        $sub = "Re: " . $request->original_subject;
+        $name = "Client";
+
+        Mail::to($email)->send(new mailReply($msg, $sub, $name, $email));
+        return back()->with('success', 'Reply sent successfully to ' . $email);
+    }
 }
