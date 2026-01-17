e0ab26f100b431c44cea994130fc2bbd97325e9b
amitnayak100312 <amitnayak121003@gmail.com>
2026-01-17 15:07:48 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,27 @@ Route::get('/', function () {
 
 // Route::view('/','components.header');
 
-Route::view('/addContent','back-end.addContent');
-Route::view('/dashboard','back-end.dashboard');
-Route::view('/viewreview','back-end.viewreview');
 Route::get('/viewClient', [MailController::class, 'viewMessages'])->name('view.client');
 Route::get('/viewreview', [MailController::class, 'viewReview']);
-Route::view('/review', 'components.review');
-Route::view('/contact','components.contact');
-Route::view('/about','components.aboutus2');
-Route::view('/services','components.services');
-Route::view('/portfolio','components.portfolio');
 Route::post('/admin/reply', [MailController::class, 'sendReply'])->name('admin.sendReply');
 Route::get('/admin/messages', [MailController::class, 'viewMessages'])->name('admin.messages');
 Route::post('send-mail',[MailController::class,'mailApp']);
 Route::get('/review', [ReviewController::class, 'index'])->name('reviews.index');
 Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
+
+
+//all back-end view
+Route::view('/addContent','back-end.addContent');
+Route::view('/dashboard','back-end.dashboard');
+Route::view('/viewreview','back-end.viewreview');
+
+//all page userside view
+
+Route::view('/review', 'components.review');
+Route::view('/contact','components.contact');
+Route::view('/about','components.aboutus2');
+Route::view('/services','components.services');
+Route::view('/portfolio','components.portfolio');
 
 //WhatsApp Messages
